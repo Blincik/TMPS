87a90012e6b9e2301faa404214a6e40768615dff
Blincik <75649885+Blincik@users.noreply.github.com>
2024-05-30 17:09:34 +0300

    Add files via upload

---
+++ Sabloan_de_comportament_TMPS/src/main/java/org/example/PlayCommand.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.Toy;
+
+public class PlayCommand implements Command {
+    private Toy toy;
+    public PlayCommand(Toy toy) {
+        this.toy = toy;
+    }
+    @Override
+    public void execute() {
+        toy.performPlay();
+    }
+}
